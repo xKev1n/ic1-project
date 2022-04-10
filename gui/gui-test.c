8d8165c56dbf51ce53d06d85ab318e7f258c3c00
rosa-martin <matkoro@gmail.com>
2022-04-10 19:01:29 +0200

    GUI

--- gui/gui-test.c
+++ gui/gui-test.c
@@ -1,4 +1,11 @@
 #include <gtk/gtk.h>
+#include <unistd.h>
+
+char* getTime(){
+	return NULL;
+
+
+}
 
 
 int main(int argc, char* argv[])
@@ -7,11 +14,30 @@ int main(int argc, char* argv[])
 
     GtkWidget *window;
     GtkWidget *image;
+    GtkWidget *fixed;
     GdkRGBA color;
+    GtkWidget* label;
+
+    double OGtimeSecs = 300;
+    double currTimeSecs = OGtimeSecs;
+    char currTimeStr[20];
+    sprintf(currTimeStr, "%f", currTimeSecs / 60);
+    char textus1[200] = "<b><span foreground=\"red\">";
+    char textus2[20] = "</span></b>";
+    strcat(textus1, currTimeStr);
+    printf("%s\n", textus1);
+    strcat(textus1, textus2);
+    printf("%s\n", textus1);
+
+    PangoAttrList *attrList = pango_attr_list_new();
+    PangoAttribute *fontsize = pango_attr_size_new_absolute(60 * PANGO_SCALE);
+
 
     color.red = 2.4;
     color.green = 3.1;
     color.blue = 3.1;
+
+    pango_attr_list_insert(attrList, fontsize);
 
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
      
@@ -24,11 +50,20 @@ int main(int argc, char* argv[])
 
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-    image = gtk_image_new_from_file("hacked.png");
+    fixed = gtk_fixed_new();
+	
+    image = gtk_image_new_from_file("resources/images/hacked.png");
 
-    //gtk_image_set_pixel_size(GTK_IMAGE(image), 1000);
+    label = gtk_label_new(NULL);
+    gtk_label_set_markup(GTK_LABEL(label), textus1);
+    gtk_label_set_attributes(GTK_LABEL(label), attrList);
 
-    gtk_container_add(GTK_CONTAINER(window), image);
+    pango_attr_list_unref(attrList);
+
+    gtk_fixed_put(GTK_FIXED(fixed), image, 0, 0);
+    gtk_fixed_put(GTK_FIXED(fixed), label, 530, 650);
+
+    gtk_container_add(GTK_CONTAINER(window), fixed);
 
     gtk_widget_show_all(window);
 
