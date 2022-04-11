ac54868d876c738c18d7fedf7418d9deae8ff750
rosa-martin <matkoro@gmail.com>
2022-04-11 11:26:23 +0200

    updated some stuff

--- gui/gui-test.c
+++ gui/gui-test.c
@@ -1,8 +1,33 @@
 #include <gtk/gtk.h>
 #include <unistd.h>
 
-char* getTime(){
-	return NULL;
+//global vars
+
+const int end = 300;
+int mins = end / 60;
+int sec = end % 60;
+
+
+void getTime(char txt[200]){
+	char minsStr[20];
+    char secStr[20];
+    sprintf(minsStr, "%d", mins);
+    sprintf(secStr, "%d", sec);
+    char textus2[20] = "</span></b>";
+    const char* colon = ":";
+    const char* colonWithZero = ":0";
+    strcat(txt, minsStr);
+    //printf("%s\n", txt);
+    if(sec < 10){
+        strcat(txt, colonWithZero);
+    } else {
+        strcat(txt, colon);
+    }
+    //printf("%s\n", txt);
+    strcat(txt, secStr);
+    //printf("%s\n", txt);
+    strcat(txt, textus2);
+    //printf("%s\n", txt);
 
 
 }
@@ -18,16 +43,9 @@ int main(int argc, char* argv[])
     GdkRGBA color;
     GtkWidget* label;
 
-    double OGtimeSecs = 300;
-    double currTimeSecs = OGtimeSecs;
-    char currTimeStr[20];
-    sprintf(currTimeStr, "%f", currTimeSecs / 60);
     char textus1[200] = "<b><span foreground=\"red\">";
-    char textus2[20] = "</span></b>";
-    strcat(textus1, currTimeStr);
-    printf("%s\n", textus1);
-    strcat(textus1, textus2);
-    printf("%s\n", textus1);
+    getTime(textus1);
+
 
     PangoAttrList *attrList = pango_attr_list_new();
     PangoAttribute *fontsize = pango_attr_size_new_absolute(60 * PANGO_SCALE);
@@ -46,9 +64,9 @@ int main(int argc, char* argv[])
     gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
     gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
     gtk_widget_override_background_color(window, GTK_STATE_NORMAL, &color);
-    //gtk_window_set_decorated(GTK_WINDOW(window), FALSE);
+    gtk_window_set_decorated(GTK_WINDOW(window), FALSE);
 
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    //g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
     fixed = gtk_fixed_new();
 	
